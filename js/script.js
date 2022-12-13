ff672d6eedf49bcb0468116ab16d38aa8a33aabb
IT21167614 <92454064+Nimsaramahagedara@users.noreply.github.com>
2022-12-13 14:02:08 +0530

    mainSec

--- js/script.js
+++ js/script.js
@@ -7,14 +7,16 @@ function menu(){
         navLinks.style.visibility = "hidden";
         navLinks.style.marginTop = "-273px";
         
-        /*navLinks.style.display = "none";*/
-        shop.style.display = "none";
+        /*navLinks.style.display = "none";
+        shop.style.visibility = "hidden";*/
         menuBtn.className ="menu-btn fa-solid fa-bars";
+        shop.style.marginTop = "0px";
     }else{
        /* navLinks.style.display = "block";*/
        navLinks.style.visibility = "visible";
-        navLinks.style.marginTop = "0px"
-        shop.style.display = "block";
+        navLinks.style.marginTop = "60px"
+       /* shop.style.visibility = "visible";
+        shop.style.marginTop = "330px";*/
         menuBtn.className = "menu-btn fa-solid fa-xmark";
     }
 }
